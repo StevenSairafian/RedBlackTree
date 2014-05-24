26660217b7c516604ae43c86dd7e88a54daded62
Stevo <smsairafian@csupomona.edu>
2014-05-24 16:36:23 -0700

    Added find method. Began work on add method

--- CS241-Project3/src/rbTree/RBTree.java
+++ CS241-Project3/src/rbTree/RBTree.java
@@ -8,4 +8,36 @@ public class RBTree<V extends Comparable<V>> {
     public RBTree(){
 	root = null;
     }
+    
+    public void add(V element){
+    	RBNode<V> addThis = new RBNode<V>(element);
+    	if(root == null){
+    		root = addThis;
+    		root.isRed = false;
+    		return;
+    	}
+    	
+    }
+    
+    private RBNode find(V element){
+    	if(root.data == null){
+    	return null;
+    	}
+    	if(root.data.compareTo(element) == 0){
+    		return root;
+    	}
+    	RBNode<V> index = root;
+    	while(index.data != null){
+    		if(index.data.compareTo(element) == 0){
+    			return index;
+    		}
+    	else if(index.data.compareTo(element) < 0){
+    			index = index.left;
+    		}
+    		else{
+    			index = index.right;
+    		}
+    	}
+    	return null;
+    }
 }
