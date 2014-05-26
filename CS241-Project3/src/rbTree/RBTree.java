e97794e4f9883169074f61ea8f1a4eeb9cd544f3
Stevo <smsairafian@csupomona.edu>
2014-05-26 02:46:05 -0700

    added rotations

--- CS241-Project3/src/rbTree/RBTree.java
+++ CS241-Project3/src/rbTree/RBTree.java
@@ -20,9 +20,18 @@ public class RBTree<V extends Comparable<V>> {
 			return;
 		}
 
+		RBNode<V> position = findAdd(element);
+		position.data = element;
+		position.left = leaf;
+		position.right = leaf;
+		position.isRed = true;
+		
+		if(position.parent.isRed){
+			rotate(position.parent);
+		}
 	}
 
-	private void findAdd(V element) {
+	private RBNode<V> findAdd(V element) {
 		RBNode<V> index = root;
 		// while not at a leaf
 		while (index.data != null) {
@@ -37,18 +46,20 @@ public class RBTree<V extends Comparable<V>> {
 				index.isRed = true;
 				index.left.isRed = false;
 				index.right.isRed = false;
-			}
 
-			if (index.parent.isRed) {
-				// single or double rotate
-				if (isOutside(index)) {
-					singleRotate(index);
-				} else {
-					doubleRotate(index);
+				if (index.parent.isRed) {
+					// single or double rotate
+					/*if (isOutside(index)) {
+						singleRotate(index);
+					} else {
+						doubleRotate(index);
+					}*/
+					rotate(index);
 				}
 			}
 
 		}
+		return index;
 	}
 
 	private boolean isOutside(RBNode<V> node) {
@@ -84,28 +95,80 @@ public class RBTree<V extends Comparable<V>> {
 		}
 		return null;
 	}
-	
-	private void rightRotate(RBNode<V> node){
+
+	private void rightRotate(RBNode<V> node) {
 		RBNode<V> temp = node.parent;
-		//if the parent node is the left child of the grandparent
-		if(node.parent.parent.left == node.parent){
+		// if the parent node is the left child of the grandparent
+		if (node.parent.parent.left == node.parent) {
 			node.parent.parent.left = node;
 		}
-		//otherwise the parent node is the right child of the grandparent
-		else{
-			//right rotate on right subtree
+		// otherwise the parent node is the right child of the grandparent
+		else {
+			// right rotate on right subtree
 			node.parent.parent.right = node;
 		}
-			//right child of node becomes left child of parent node
-			temp.left = node.right;
-			//set right child of node to ex parent
-			node.right = temp;
-			//recolor
-			node.right.isRed = true;
-			node.isRed = false;
+		// right child of node becomes left child of parent node
+		temp.left = node.right;
+		// set right child of node to ex parent
+		node.right = temp;
+		// recolor
+		node.right.isRed = true;
+		node.isRed = false;
 	}
 	
-	private void doubleRotate(RBNode<V> node){
+	private void leftRotate(RBNode<V> node){
+		RBNode<V> temp = node.parent;
 		
+	}
+
+	private void rotate(RBNode<V> node) {
+		switch(position(node)){
+		case 1:
+			rightRotate(node);
+			break;
+		case 2:
+			leftRotate(node);
+			break;
+		case 3:
+			leftRightRotate(node);
+			break;
+		case 4:
+			rightLeftRotate(node);
+			break;
+		default:
+			break;
+			
+		}
+	}
+	private void leftRightRotate(RBNode<V> node){
+		leftRotate(node);
+		rightRotate(node);
+		node.isRed = false;
+		node.left.isRed = true;
+		node.right.isRed = true;
+	}
+	
+	private void rightLeftRotate(RBNode<V> node){
+		rightRotate(node);
+		leftRotate(node);
+		node.isRed = false;
+		node.left.isRed = true;
+		node.right.isRed = true;
+	}
+	private int position(RBNode<V> node){
+		if (node == root) {
+			return -1;
+		}
+		if (node == root.left || node.parent.parent.left.left == node) {
+			return 1;
+		}
+		if(node == root.right || node.parent.parent.right.right == node){
+			return 2;
+		}
+		if(node == node.parent.left.right){
+			return 3;
+		}
+
+		return 4;
 	}
 }
